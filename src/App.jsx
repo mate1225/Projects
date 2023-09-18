e13d85f31002bc38d64191c0faeedd619014bac0
mate1225 <mate981225@gmail.com>
2023-09-18 15:39:43 +0200

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import "./App.css";
 //jsx
 
-import FrontendMentor from "./components/FrontendMentor";
+import FrontendMentor from "./components/Frontend_Mentor/FrontendMentor";
 function App() {
   return (
     <article className=" m-5 flex  items-center justify-center lg:m-5">
